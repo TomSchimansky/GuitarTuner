4896f6c95d08cf264a6956b251a0043f15ba3b07
Tom Schimansky <tomschimansk@gmail.com>
2021-03-14 13:43:51 +0100

    AudioAnalyzer changes

--- tuner_audio/audio_analyzer.py
+++ tuner_audio/audio_analyzer.py
@@ -14,6 +14,7 @@ class AudioAnalyzer(Thread):
 
         queue = ProtectedList()
         analyzer = AudioAnalyzer(queue)
+        analyzer.start()
 
         while True:
             print("Loudest Frequency:", queue.get())
